2e9b4f3c6912af40537b880fa19bdb02ea94dcca
VynixiusWare Studios <98066737+VynixiusWare@users.noreply.github.com>
2024-06-20 08:26:42 +0800

    Update SolarisHub.lua

--- SolarisHub.lua
+++ SolarisHub.lua
@@ -34,7 +34,7 @@ local function PrisonLife()
       game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(918.77,100,2379.07)
     end)
 
-    LocalPlayerTab.NewButton("Serverhop", "Change the server", function()
+    LocalPlayerTab.newButton("Serverhop", "Change the server", function()
       local Http = game:GetService("HttpService") local TPS = game:GetService("TeleportService") local Api = "https://games.roblox.com/v1/games/" local _place = game.PlaceId local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100" function ListServers(cursor) local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or "")) return Http:JSONDecode(Raw) end local Server, Next; repeat local Servers = ListServers(Next) Server = Servers.data[1] Next = Servers.nextPageCursor until Server TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
     end)
 
